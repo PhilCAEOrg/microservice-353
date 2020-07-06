068f5f0a11c45745afe6e60faed790a12d34de6b
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-06 07:33:48 +0000

    Added GET for todo items

---
+++ db/353_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 353, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table todo_items.
+--
+CREATE TABLE commedit.todo_items (
+  item_name VARCHAR(255) ,
+  id INTEGER ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
